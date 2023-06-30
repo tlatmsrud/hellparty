b624e70cf716d4fcb681fa205effc5b84bfbe643
tlatmsrud <tlatmsrud@naver.com>
2023-06-30 21:25:45 +0900

    :sparkles: Add-member-health-info-entity
    사용자의 헬스 관련 정보 엔티티 추가

---
+++ src/main/java/com/hellparty/domain/MemberHealth.java
@@ -0,0 +1,48 @@
+package com.hellparty.domain;
+
+import com.hellparty.domain.embedded.Address;
+import com.hellparty.enums.Division;
+import jakarta.persistence.*;
+
+import java.sql.Time;
+
+/**
+ * title        : 사용자 헬스 정보
+ * author       : sim
+ * date         : 2023-06-30
+ * description  : 사용자의 헬스 관련 정보 엔티티
+ */
+
+@Entity
+@Table(name = "TBL_MEMBER_HEALTH")
+public class MemberHealth {
+
+    @Id @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member memberId;
+
+    @Column(name = "EXEC_STT_TIME")
+    @Temporal(TemporalType.TIME)
+    private Time execStartTime;
+
+    @Column(name = "EXEC_END_TIME")
+    @Temporal(TemporalType.TIME)
+    private Time execEndTime;
+
+    @Column(name = "DIV")
+    @Enumerated(EnumType.STRING)
+    private Division div;
+
+    @Column(name = "EXEC_AREA")
+    private Long execArea;
+
+    @Embedded
+    private Address gymAddress;
+
+    @Column(name = "SPCL_NOTE")
+    private String spclNote;
+}
