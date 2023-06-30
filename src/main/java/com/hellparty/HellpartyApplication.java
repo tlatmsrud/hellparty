520d0c02993fed1e583af61f702c796f119d59d6
tlatmsrud <tlatmsrud@naver.com>
2023-06-30 17:38:47 +0900

    :tada: Init-project

---
+++ src/main/java/com/hellparty/HellpartyApplication.java
@@ -0,0 +1,13 @@
+package com.hellparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellpartyApplication.class, args);
+	}
+
+}
