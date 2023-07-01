b1bdfb40974dc619358f0bfb772c4c942b4f6f14
tlatmsrud <tlatmsrud@naver.com>
2023-07-01 23:51:15 +0900

    :art: Add-update

--- src/main/java/com/hellparty/service/MemberService.java
+++ src/main/java/com/hellparty/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.hellparty.service;
 
+import com.hellparty.domain.Member;
+import com.hellparty.dto.MemberDTO;
+import com.hellparty.exception.NotFoundException;
 import com.hellparty.repository.MemberRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,5 +23,22 @@ public class MemberService {
     public boolean isExistMemberByEmail(String email){
 
         return memberRepository.existsMemberByEmail(email);
+    }
+
+    public void update(MemberDTO.Update memberDTO){
+
+        Member findMember = memberRepository.findById(memberDTO.getId())
+                .orElseThrow(() -> new NotFoundException("사용자를 찾을 수 없습니다."));
+
+        findMember.updateMember(
+                memberDTO.getNickname()
+                ,memberDTO.getEmail()
+                ,memberDTO.getHeight()
+                ,memberDTO.getWeight()
+                ,memberDTO.getAge()
+                ,memberDTO.getSex()
+                ,memberDTO.getMbti()
+                ,memberDTO.getProfileUrl()
+        );
     }
 }
