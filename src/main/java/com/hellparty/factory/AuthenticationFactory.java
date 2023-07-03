10c4f8616d8ef98814ad5c955b225d60b1d3a131
tlatmsrud <tlatmsrud@naver.com>
2023-07-03 16:29:16 +0900

    :sparkles: Create-AuthenticationFactory
    Authentication 객체를 생성하는 팩토리 클래스

---
+++ src/main/java/com/hellparty/factory/AuthenticationFactory.java
@@ -0,0 +1,24 @@
+package com.hellparty.factory;
+
+import com.hellparty.enums.Role;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.Collections;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-03
+ * description  :
+ */
+public class AuthenticationFactory {
+
+    public static Authentication getAuthentication(String email){
+        return new UsernamePasswordAuthenticationToken(
+                email
+                , ""
+                , Collections.singleton(new SimpleGrantedAuthority(Role.ROLE_USER.name()))
+        );
+    }
+}
