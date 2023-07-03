bd80242f2e8f5574330a90262e9462355ee26135
tlatmsrud <tlatmsrud@naver.com>
2023-07-03 16:28:28 +0900

    :wrench: Update-Security-Setting
    1. Session 사용 X
    2. Jwt 인증 필터 추가

--- src/main/java/com/hellparty/config/SecurityConfig.java
+++ src/main/java/com/hellparty/config/SecurityConfig.java
@@ -1,19 +1,20 @@
 package com.hellparty.config;
 
+import com.hellparty.filter.JwtAuthorizationFilter;
 import com.hellparty.jwt.JwtProvider;
 import com.hellparty.oauth.CustomSuccessHandler;
-import com.hellparty.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 /**
- * title        :
+ * title        : SpringSecurity 설정
  * author       : sim
  * date         : 2023-06-30
  * description  :
@@ -22,11 +23,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig{
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 
     private final JwtProvider jwtProvider;
 
@@ -38,14 +34,18 @@ public class SecurityConfig{
                 .authorizeHttpRequests()
                 .requestMatchers(new AntPathRequestMatcher("/api/login/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/login.html")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/login/**")).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .headers().frameOptions().disable() // frameOptions은 기본 Deny. h2는 iframe을 사용하기에 disable 처리
-                .and().csrf().disable();// h2 UI를 통해 DB connect 시 post 통신이 발생하면서 csrf 에러가 발생. 이에따라 disable 처리
-        http
+                .and()
+                .csrf().disable()// h2 UI를 통해 DB connect 시 post 통신이 발생하면서 csrf 에러가 발생. 이에따라 disable 처리
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 인증으로 인한 Session 미생성.
+                .and()
                 .oauth2Login()
-                .successHandler(new CustomSuccessHandler(jwtProvider));
+                .successHandler(new CustomSuccessHandler(jwtProvider))
+                .and()
+                .addFilterBefore(new JwtAuthorizationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
