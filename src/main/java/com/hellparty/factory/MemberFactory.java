226957dd67456a4cfbbdd814d597e901c6df2d4f
tlatmsrud <tlatmsrud@naver.com>
2023-07-04 02:21:00 +0900

    :sparkles: Create-MemberFactory
    Member 엔티티에 대해 DTO 객체를 생성하는 팩터리클래스

---
+++ src/main/java/com/hellparty/factory/MemberFactory.java
@@ -0,0 +1,29 @@
+package com.hellparty.factory;
+
+import com.hellparty.domain.Member;
+import com.hellparty.dto.MemberDTO;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-01
+ * description  :
+ */
+public class MemberFactory {
+
+    public static MemberDTO entityToDTO(Member member){
+        return MemberDTO.builder()
+                .id(member.getId())
+                .age(member.getAge())
+                .bodyProfileUrl(member.getBodyProfileUrl())
+                .sex(member.getSex())
+                .profileUrl(member.getProfileUrl())
+                .mbti(member.getMbti())
+                .weight(member.getWeight())
+                .height(member.getHeight())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .build();
+
+    }
+}
