8494164d0ec7bbc5a765abab15efa0e1321a89fa
tlatmsrud <tlatmsrud@naver.com>
2023-07-03 16:24:42 +0900

    :art: Update-logic
    1. 생성자 호출 시 attributes 값 수정
    2. attributes 가 수정됨에 따라 attributes에 대한 key 값을 나타내는 attributesKey 필드 추가
    3. of에 대한 스태틱 메서드의 usernameAttributeName을 "email"로 변경.

--- src/main/java/com/hellparty/oauth/OAuth2Attributes.java
+++ src/main/java/com/hellparty/oauth/OAuth2Attributes.java
@@ -18,42 +18,46 @@ import java.util.Map;
 public class OAuth2Attributes {
 
     private final Map<String, Object> attributes;
+    private final String attributesKey;
     private final String nickname;
     private final String email;
     private final String profileUrl;
 
-    public OAuth2Attributes(Map<String, Object> attributes, String nickname, String email, String profileUrl) {
+    public OAuth2Attributes(Map<String, Object> attributes, String attributesKey, String nickname, String email, String profileUrl) {
         this.attributes = attributes;
+        this.attributesKey = attributesKey;
         this.nickname = nickname;
         this.email = email;
         this.profileUrl = profileUrl;
     }
 
-    public static OAuth2Attributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+    public static OAuth2Attributes of(String registrationId, Map<String, Object> attributes) {
         if (registrationId.equals(OAuthType.KAKAO.getValue())) {
-            return ofKakao(userNameAttributeName, attributes);
+            return ofKakao("email", attributes);
         }
-        return ofNaver(userNameAttributeName,attributes);
+        return ofNaver("email",attributes);
     }
 
-    private static OAuth2Attributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
-        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get(userNameAttributeName);
+    private static OAuth2Attributes ofKakao(String attributesKey, Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
         Map<String, String> profile = (Map<String, String>) kakaoAccount.get("profile");
 
-        return new OAuth2Attributes(attributes,
+        return new OAuth2Attributes(kakaoAccount,
+                attributesKey,
                 profile.get("nickname"),
                 (String) kakaoAccount.get("email"),
                 profile.get("profile_image_url")
         );
     }
 
-    private static OAuth2Attributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
-        Map<String, String> response = (Map<String, String>) attributes.get(userNameAttributeName);    // 네이버에서 받은 데이터에서 프로필 정보다 담긴 response 값을 꺼낸다.
+    private static OAuth2Attributes ofNaver(String attributesKey, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");    // 네이버에서 받은 데이터에서 프로필 정보다 담긴 response 값을 꺼낸다.
 
-        return new OAuth2Attributes(attributes,
-                response.get("nickname"),
-                response.get("email"),
-                response.get("profile_image"));
+        return new OAuth2Attributes(response,
+                attributesKey,
+                (String)response.get("nickname"),
+                (String)response.get("email"),
+                (String)response.get("profile_image"));
     }
 
     public Member toMemberEntity(){
