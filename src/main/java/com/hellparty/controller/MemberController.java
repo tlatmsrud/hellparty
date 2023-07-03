976cbf60864dab42efbedbe1c07cc2c0078d24c7
tlatmsrud <tlatmsrud@naver.com>
2023-07-04 02:09:22 +0900

    :art: Add-Detail
    사용자 정보 조회 메서드 생성

--- src/main/java/com/hellparty/controller/MemberController.java
+++ src/main/java/com/hellparty/controller/MemberController.java
@@ -1,28 +1,39 @@
 package com.hellparty.controller;
 
+import com.hellparty.annotation.LoginMemberId;
 import com.hellparty.dto.MemberDTO;
 import com.hellparty.service.MemberService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * title        :
+ * title        : MemberController
  * author       : sim
  * date         : 2023-07-01
- * description  :
+ * description  : 사용자 컨트롤러
  */
 
-@Controller
+@RestController
 @RequestMapping("/api/member")
 @AllArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public MemberDTO detail(@LoginMemberId Long id){
+
+        return memberService.getDetail(id);
+
+    }
+
     @PutMapping
+    @ResponseStatus(HttpStatus.OK)
     public void update(MemberDTO.Update updateMemberDTO){
         memberService.update(updateMemberDTO);
     }
+
+
 }
