f362e4cec17429f9dbede8782691d4776245af28
tlatmsrud <tlatmsrud@naver.com>
2023-07-03 16:30:22 +0900

    :sparkles: Create-Role-Enum

---
+++ src/main/java/com/hellparty/enums/Role.java
@@ -0,0 +1,13 @@
+package com.hellparty.enums;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-03
+ * description  :
+ */
+public enum Role {
+
+    ROLE_USER
+    ,ROLE_ADMIN
+}
