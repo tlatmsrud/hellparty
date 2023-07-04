0c0874d4253c5ed5aa5b9c6f70155a264d85b192
tlatmsrud <tlatmsrud@naver.com>
2023-07-04 20:22:06 +0900

    :bug: Fix-ControllerAdvice-binding-error
    ControllerAdvice에서 에러 DTO의 값이 바인딩 되지 않아 내부 에러가 발생함.
    확인 결과 Getter 메서드를 입력하지 않아 errMessage를 DTO에 바인딩하지 못한것으로 확인

--- src/main/java/com/hellparty/dto/ErrorResponseDTO.java
+++ src/main/java/com/hellparty/dto/ErrorResponseDTO.java
@@ -1,11 +1,15 @@
 package com.hellparty.dto;
 
+import lombok.Getter;
+
 /**
  * title        :
  * author       : sim
  * date         : 2023-07-01
  * description  :
  */
+
+@Getter
 public class ErrorResponseDTO {
 
     private String errMessage;
