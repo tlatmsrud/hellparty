5de052e6e58cf2b47d08da196821fbd9a5d68972
tlatmsrud <tlatmsrud@naver.com>
2023-07-04 22:55:38 +0900

    :white_check_mark: Test-HealthDetail-Get,Put

--- src/test/java/com/hellparty/controller/MemberControllerTest.java
+++ src/test/java/com/hellparty/controller/MemberControllerTest.java
@@ -1,10 +1,13 @@
 package com.hellparty.controller;
 
+import auth.TestMemberAuth;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hellparty.domain.embedded.Address;
+import com.hellparty.domain.embedded.BigThree;
 import com.hellparty.dto.MemberDTO;
+import com.hellparty.dto.MemberHealthDTO;
+import com.hellparty.enums.Division;
 import com.hellparty.enums.Sex;
-import com.hellparty.exception.NotFoundException;
-import com.hellparty.filter.JwtAuthorizationFilter;
 import com.hellparty.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,10 +16,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.sql.Time;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -24,10 +30,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 /**
- * title        :
+ * title        : Member Controller Mvc Test
  * author       : sim
  * date         : 2023-07-01
- * description  :
+ * description  : 사용자 컨트롤러 테스트 클래스
  */
 
 
@@ -41,67 +47,99 @@ class MemberControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final Long VALID_MEMBER_ID = 1L;
 
-    private MemberDTO.Update VALID_UPDATE_REQUEST = MemberDTO.Update.builder()
-            .id(1L).age(10).height(170).weight(50.1).sex(Sex.M)
+    private final MemberDTO.Update UPDATE_DETAIL_REQUEST = MemberDTO.Update.builder()
+            .age(10).height(170).weight(50.1).sex(Sex.M)
             .email("test@naver.com").nickname("nickname").profileUrl("test.url")
             .build();
 
-    private MemberDTO.Update INVALID_UPDATE_REQUEST = MemberDTO.Update.builder()
-            .id(1000L).build();
+    private final MemberHealthDTO.Update UPDATE_HEALTH_DETAIL_REQUEST = MemberHealthDTO.Update.builder()
+            .execStartTime(Time.valueOf("19:00:00"))
+            .execEndTime(Time.valueOf("20:00:00"))
+            .bigThree(BigThree.builder()
+                    .squat(100)
+                    .daedlift(110)
+                    .benchPress(80)
+                    .build())
+            .execArea(123L)
+            .healthMotto("뇌는 근육으로 이루어져있다.")
+            .gymAddress(Address.builder()
+                    .y(1L)
+                    .x(2L)
+                    .address("서울시 중랑구")
+                    .placeName("중랑헬스장")
+                    .build())
+            .spclNote("특이사항")
+            .div(Division.THREE)
+            .build();
 
     @BeforeEach
     void setUp(){
 
-        willDoNothing()
-                .given(memberService).update(VALID_UPDATE_REQUEST);
+        willDoNothing().given(memberService)
+                .updateDetail(eq(VALID_MEMBER_ID), any(MemberDTO.Update.class));
 
-        willThrow(new NotFoundException("사용자를 찾을 수 없습니다."))
-                .given(memberService).update(INVALID_UPDATE_REQUEST);
+        given(memberService.getDetail(VALID_MEMBER_ID)).willReturn(
+                MemberDTO.builder()
+                        .id(VALID_MEMBER_ID)
+                        .nickname("테스터")
+                        .build()
+        );
 
-        given(memberService.getDetail(1L)).willReturn(
-                MemberDTO.builder().id(1L).build()
+        given(memberService.getHealthDetail(VALID_MEMBER_ID)).willReturn(
+                MemberHealthDTO.builder()
+                        .id(VALID_MEMBER_ID)
+                        .div(Division.THREE)
+                        .execArea(123L)
+                        .build()
         );
     }
 
     @Test
-    @WithMockUser(roles = "USER")
+    @TestMemberAuth
     void updateWithValidRequest() throws Exception {
         mockMvc.perform(
                 put("/api/member")
-                        .accept(MediaType.APPLICATION_JSON_UTF8)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(VALID_UPDATE_REQUEST))
-                        .header("Authorization", "TEST_TOKEN")
-        ).andExpect(status().isOk());
+                        .content(objectMapper.writeValueAsString(UPDATE_DETAIL_REQUEST))
+                        .with(csrf())
+        ).andExpect(status().isNoContent());
     }
 
     @Test
-    @WithMockUser(roles = "USER")
-    void updateWithInvalidRequest() throws Exception {
-        mockMvc.perform(
-                put("/api/member")
-                        .accept(MediaType.APPLICATION_JSON_UTF8)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(INVALID_UPDATE_REQUEST))
-                        .header("Authorization", "TEST_TOKEN")
-        ).andExpect(status().isNotFound());
-    }
-
-    @Test
-    @WithMockUser(roles = "USER")
+    @TestMemberAuth
     void getDetail() throws Exception {
         mockMvc.perform(
-                get("/api/memberL")
-                        .accept(MediaType.APPLICATION_JSON_UTF8)
-                        .header("Authorization", "TEST_TOKEN")
-                        .param("id","1")
+                get("/api/member")
+                        .accept(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isOk())
-                .andExpect(content().string("1"));
+                .andExpect(content().string(containsString("테스터")));
+    }
+
+    @Test
+    @TestMemberAuth
+    void getHealthDetail() throws Exception{
+        mockMvc.perform(
+                get("/api/member/health")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("THREE")));
+    }
+
+    @Test
+    @TestMemberAuth
+    void updateHealthDetail() throws Exception{
+        mockMvc.perform(
+                put("/api/member/health")
+                        .content(objectMapper.writeValueAsString(UPDATE_HEALTH_DETAIL_REQUEST))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+        ).andExpect(status().isNoContent());
     }
 }
