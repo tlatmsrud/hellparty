84d7503179cc5a2b847b8c61ed22c3688b0a7dc4
tlatmsrud <tlatmsrud@naver.com>
2023-07-04 21:18:10 +0900

    :white_check_mark: Test-MemberService

--- src/test/java/com/hellparty/service/MemberServiceTest.java
+++ src/test/java/com/hellparty/service/MemberServiceTest.java
@@ -1,11 +1,21 @@
 package com.hellparty.service;
 
+import com.hellparty.domain.Member;
+import com.hellparty.domain.MemberHealth;
+import com.hellparty.domain.embedded.BigThree;
+import com.hellparty.dto.MemberDTO;
+import com.hellparty.dto.MemberHealthDTO;
+import com.hellparty.enums.Division;
+import com.hellparty.enums.Sex;
+import com.hellparty.exception.NotFoundException;
+import com.hellparty.repository.MemberHealthRepository;
 import com.hellparty.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -17,20 +27,52 @@ import static org.mockito.Mockito.mock;
  */
 class MemberServiceTest {
 
-    private MemberService memberService;
-    private MemberRepository memberRepository;
-
+    private MemberHealthRepository memberHealthRepository = mock(MemberHealthRepository.class);
+    private MemberRepository memberRepository = mock(MemberRepository.class);
+    private MemberService memberService = new MemberService(memberRepository, memberHealthRepository);
     private String VALID_EMAIL = "test@naver.com";
-
     private String INVALID_EMAIL = "no@naver.com";
+
+    private final Long VALID_ID = 1L;
+    private final Long INVALID_ID = 1000L;
+    private final MemberDTO.Update UPDATE_REQUEST = MemberDTO.Update.builder()
+            .nickname("update-nickname")
+            .age(10)
+            .sex(Sex.M)
+            .build();
 
     @BeforeEach
     void setUp(){
-        memberRepository = mock(MemberRepository.class);
-        memberService = new MemberService(memberRepository);
 
-        given(memberRepository.existsMemberByEmail(VALID_EMAIL)).willReturn(true);
-        given(memberRepository.existsMemberByEmail(INVALID_EMAIL)).willReturn(false);
+        given(memberRepository.existsMemberByEmail(VALID_EMAIL))
+                .willReturn(true);
+
+        given(memberRepository.existsMemberByEmail(INVALID_EMAIL))
+                .willReturn(false);
+
+        given(memberRepository.findById(VALID_ID))
+                .willReturn(Optional.of(
+                        Member.builder()
+                                .id(VALID_ID)
+                                .email("test@naver.com")
+                                .build()
+                ));
+
+        given(memberHealthRepository.findById(VALID_ID))
+                .willReturn(Optional.of(
+                        MemberHealth.builder()
+                                .id(VALID_ID)
+                                .div(Division.THREE)
+                                .bigThree(BigThree.builder()
+                                        .squat(100.5)
+                                        .daedlift(100)
+                                        .benchPress(100)
+                                        .build())
+                                .build()
+                ));
+
+        given(memberHealthRepository.findById(INVALID_ID))
+                .willReturn(Optional.empty());
 
     }
     @Test
@@ -43,6 +85,31 @@ class MemberServiceTest {
     void isExistMemberByEmailWithInvalidEmail() {
 
         assertThat(memberService.isExistMemberByEmail(INVALID_EMAIL)).isFalse();
+    }
+
+    @Test
+    void getDetail(){
+        MemberDTO memberDTO = memberService.getDetail(VALID_ID);
+        assertThat(memberDTO.getId()).isEqualTo(VALID_ID);
+        assertThat(memberDTO.getEmail()).isEqualTo("test@naver.com");
+    }
+
+    @Test
+    void getHealthDetailWithValidId(){
+        MemberHealthDTO memberHealthDTO = memberService.getHealthDetail(VALID_ID);
+        assertThat(memberHealthDTO.getId()).isEqualTo(VALID_ID);
+        assertThat(memberHealthDTO.getBigThree().getSquat()).isEqualTo(100.5);
+    }
+
+    @Test
+    void getHealthDetailWithInvalidId(){
+        assertThatThrownBy(()-> memberService.getHealthDetail(INVALID_ID))
+                .isInstanceOf(NotFoundException.class);
+
+    }
+    @Test
+    void updateDetail(){
+        assertThatNoException().isThrownBy(() -> memberService.update(VALID_ID, UPDATE_REQUEST));
     }
 
 
