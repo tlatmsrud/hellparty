2ee635ce02287d800ffdcf48f7ea18437df085b2
tlatmsrud <tlatmsrud@naver.com>
2023-07-06 18:04:18 +0900

    :sparkles: Create-Repository

---
+++ src/main/java/com/hellparty/repository/PartnerRepository.java
@@ -0,0 +1,13 @@
+package com.hellparty.repository;
+
+import com.hellparty.domain.Partner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-06
+ * description  :
+ */
+public interface PartnerRepository extends JpaRepository<Long, Partner> {
+}
