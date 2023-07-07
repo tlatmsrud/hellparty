7b115858c928607c2af65db5c86f66e4ca17b2a6
tlatmsrud <tlatmsrud@naver.com>
2023-07-07 11:36:30 +0900

    :test_tube: Add-Negative-Test

---
+++ src/test/java/com/hellparty/service/PartnerRequestServiceTest.java
@@ -0,0 +1,68 @@
+package com.hellparty.service;
+
+import attributes.TestFixture;
+import com.hellparty.domain.PartnerRequest;
+import com.hellparty.exception.BadRequestException;
+import com.hellparty.exception.NotFoundException;
+import com.hellparty.repository.MemberRepository;
+import com.hellparty.repository.PartnerRequestRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-07
+ * description  :
+ */
+class PartnerRequestServiceTest implements TestFixture {
+    private final PartnerRequestRepository partnerRequestRepository = mock(PartnerRequestRepository.class);
+    private final MemberRepository memberRepository = mock(MemberRepository.class);
+    private final PartnerRequestService partnerRequestService = new PartnerRequestService(partnerRequestRepository, memberRepository);
+
+    @BeforeEach
+    void setUp(){
+        given(memberRepository.findById(LOGIN_MEMBER_ID))
+                .willReturn(Optional.of(LOGIN_MEMBER_ENTITY));
+
+        given(memberRepository.findById(VALID_MEMBER_ID))
+                .willReturn(Optional.of(VALID_MEMBER_ENTITY));
+
+        given(memberRepository.findById(NOT_LOOKING_FOR_PARTNER_MEMBER_ID))
+                .willReturn(Optional.of(NOT_LOOKING_FOR_PARTNER_MEMBER_ENTITY));
+
+        given(memberRepository.findById(INVALID_MEMBER_ID))
+                .willThrow(new NotFoundException("대상 사용자를 찾을 수 없습니다. 다시 시도해주세요."));
+
+        given(memberRepository.findById(NOT_LOOKING_FOR_PARTNER_MEMBER_ID))
+                .willThrow(new BadRequestException("대상 사용자는 파트너를 구하지 않는 상태입니다. 다른 사용자를 찾아보세요."));
+    }
+    @Test
+    void requestPartnerWithValidMemberId() {
+        partnerRequestService.requestPartner(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
+        verify(partnerRequestRepository).save(any(PartnerRequest.class));
+    }
+
+    @Test
+    void requestPartnerWithInvalidMemberId() {
+        assertThatThrownBy(() -> partnerRequestService.requestPartner(LOGIN_MEMBER_ID, INVALID_MEMBER_ID))
+                .isInstanceOf(NotFoundException.class);
+
+    }
+
+    @Test
+    void requestPartnerWithNotLookingForPartnerMember() {
+        assertThatThrownBy(()-> partnerRequestService.requestPartner(LOGIN_MEMBER_ID, NOT_LOOKING_FOR_PARTNER_MEMBER_ID))
+                .isInstanceOf(BadRequestException.class);
+
+    }
+}
