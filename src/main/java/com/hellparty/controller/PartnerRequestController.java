d2b7c9fee5446fdabc1930c5cef25c302af2229f
tlatmsrud <tlatmsrud@naver.com>
2023-07-07 16:05:32 +0900

    :sparkles: Add-getPartnerRequestList-method

--- src/main/java/com/hellparty/controller/PartnerRequestController.java
+++ src/main/java/com/hellparty/controller/PartnerRequestController.java
@@ -1,8 +1,12 @@
 package com.hellparty.controller;
 
 import com.hellparty.annotation.LoginMemberId;
+import com.hellparty.dto.PartnerRequestDTO;
 import com.hellparty.service.PartnerRequestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +25,7 @@ public class PartnerRequestController {
     private final PartnerRequestService partnerRequestService;
 
     /**
-     * 파트너 요청
+     * 파트너 요청하기
      * @param fromMemberId - 요청자 ID
      * @param toMemberId - 파트너 요청 수신 ID
      */
@@ -29,5 +33,17 @@ public class PartnerRequestController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void requestPartner(@LoginMemberId Long fromMemberId, @PathVariable("toMemberId") Long toMemberId){
         partnerRequestService.requestPartner(fromMemberId, toMemberId);
+    }
+
+    /**
+     * 나의 파트너 요청 리스트 조회
+     * @param id - 사용자 ID
+     * @param pageable - page 속성
+     * @return 페이징 처리된 파트너 요청 리스트
+     */
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Page<PartnerRequestDTO> getPartnerRequestList(@LoginMemberId Long id, @PageableDefault Pageable pageable){
+        return partnerRequestService.getPartnerRequestList(id, pageable);
     }
 }
