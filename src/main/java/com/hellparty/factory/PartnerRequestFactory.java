bf5cb10dc312cda59f77364728f4e402a44b5a91
tlatmsrud <tlatmsrud@naver.com>
2023-07-07 11:50:38 +0900

    :bulb: Update-comment

--- src/main/java/com/hellparty/factory/PartnerRequestFactory.java
+++ src/main/java/com/hellparty/factory/PartnerRequestFactory.java
@@ -5,10 +5,10 @@ import com.hellparty.domain.PartnerRequest;
 import com.hellparty.enums.PartnerResponseStatus;
 
 /**
- * title        :
+ * title        : PartnerRequest 팩토리
  * author       : sim
  * date         : 2023-07-07
- * description  :
+ * description  : PartnerRequest 엔티티에 대한 팩토리 클래스
  */
 public class PartnerRequestFactory {
 
