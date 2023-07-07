bde91af28bcf6bdd7574cc8671ec335b21388f66
tlatmsrud <tlatmsrud@naver.com>
2023-07-07 17:04:43 +0900

    :art: Add-comment-and-method
    answerPartnerRequest, isRequestForLoginMember 메서드 추가
    isRequestForLoginMember는 로그인 사용자와 파트너 요청 엔티티의 수신자 정보가 일치하는지 확인한다. 확인하지 않으면 타인의 파트너 신청을 처리할 수 있기 때문임.

--- src/main/java/com/hellparty/service/PartnerRequestService.java
+++ src/main/java/com/hellparty/service/PartnerRequestService.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 /**
  * title        : 파트너 요청 서비스
  * author       : sim
@@ -28,6 +29,11 @@ public class PartnerRequestService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * 파트너 요청하기
+     * @param fromMemberId - 요청 사용자 ID
+     * @param toMemberId - 수신 사용자 ID
+     */
     public void requestPartner(Long fromMemberId, Long toMemberId){
 
         Member fromMember = memberRepository.findById(fromMemberId)
@@ -46,7 +52,39 @@ public class PartnerRequestService {
     }
 
 
+    /**
+     * 파트너 요청 리스트 조회
+     * @param id - 사용자 ID
+     * @param pageable - 페이지 속성
+     * @return 페이징 처리된 파트너 요청 리스트
+     */
     public Page<PartnerRequestDTO> getPartnerRequestList(Long id, Pageable pageable) {
         return partnerRequestRepository.findPartnerRequestList(id, pageable);
+    }
+
+    /**
+     * 파트너 요청 응답하기
+     * @param memberId - 사용자 ID
+     * @param request - 파트너 요청 응답 Dto
+     */
+    public void answerPartnerRequest(Long memberId, PartnerRequestDTO.Answer request){
+        PartnerRequest partnerRequest = partnerRequestRepository.findById(request.getId())
+                .orElseThrow(() -> new NotFoundException("요청 데이터를 찾을 수 없습니다. 관리자에게 문의해주세요."));
+
+        if(!isRequestForLoginMember(memberId, partnerRequest)){
+            throw new BadRequestException("잘못된 요청입니다. 다시 시도해주세요.");
+        }
+
+        partnerRequest.updateStatus(request.getStatus());
+    }
+
+    /**
+     *
+     * @param memberId - 사용자 ID
+     * @param partnerRequest - 파트너 요청 엔티티
+     * @return 해당 파트너 요청의 대상이 사용자일 경우 true
+     */
+    public boolean isRequestForLoginMember(Long memberId, PartnerRequest partnerRequest){
+        return memberId.equals(partnerRequest.getToMember().getId());
     }
 }
