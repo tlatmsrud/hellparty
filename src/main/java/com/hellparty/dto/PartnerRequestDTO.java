dfefd7f4f669a843559fbffb63d4067a7e78dfad
tlatmsrud <tlatmsrud@naver.com>
2023-07-07 16:07:43 +0900

    :sparkles: Create-PartnerRequestDTO

---
+++ src/main/java/com/hellparty/dto/PartnerRequestDTO.java
@@ -0,0 +1,24 @@
+package com.hellparty.dto;
+
+import com.hellparty.enums.PartnerResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * title        : 파트너 요청 DTO
+ * author       : sim
+ * date         : 2023-07-07
+ * description  : 파트너 요청 관련 DTO
+ */
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PartnerRequestDTO {
+    private Long id;
+    private MemberDTO member;
+    private PartnerResponseStatus status;
+}
