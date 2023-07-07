fcc6ce9d0d8308037f562b4516465724ca3dc542
tlatmsrud <tlatmsrud@naver.com>
2023-07-07 10:31:24 +0900

    :sparkles: Create-TestFixture
    테스트 코드에 중복되는 픽스처가 많아 인터페이스로 관리하기 위해 생성함.

---
+++ src/test/java/attributes/TestFixture.java
@@ -0,0 +1,15 @@
+package attributes;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-07
+ * description  :
+ */
+public interface TestFixture{
+
+    Long LOGIN_MEMBER_ID = 1L;
+    Long VALID_MEMBER_ID = 2L;
+    Long NOT_LOOKING_FOR_PARTNER_MEMBER_ID = 4L;
+    Long INVALID_MEMBER_ID = 1000L;
+}
