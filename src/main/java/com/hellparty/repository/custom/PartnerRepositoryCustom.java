506bff2664b7549d4df215dc9fa4af738f28ca16
tlatmsrud <tlatmsrud@naver.com>
2023-07-09 17:16:38 +0900

    :art: Create-PartnerRepository-For-QueryDSL

---
+++ src/main/java/com/hellparty/repository/custom/PartnerRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.hellparty.repository.custom;
+
+import com.hellparty.dto.PartnerDTO;
+
+import java.util.List;
+
+/**
+ * title        : PartnerRepositoryCustom
+ * author       : sim
+ * date         : 2023-07-09
+ * description  : PartnerRepositoryCustom
+ */
+public interface PartnerRepositoryCustom {
+
+    List<PartnerDTO> getPartnerList(Long memberId);
+}
