89108ac5bbcc670fd0d4becc60ce9375c1adecd1
tlatmsrud <tlatmsrud@naver.com>
2023-07-12 01:51:41 +0900

    :sparkles: Create-PartnerFindService
    파트너 구하기 기능에 대한 서비스 클래스 생성

---
+++ src/main/java/com/hellparty/service/PartnerFindService.java
@@ -0,0 +1,27 @@
+package com.hellparty.service;
+
+import com.hellparty.dto.PartnerFindDTO;
+import com.hellparty.repository.PartnerFindRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * title        : 파트너 구하기 기능 서비스
+ * author       : sim
+ * date         : 2023-07-11
+ * description  : 파트너 구하기 기능에 대한 서비스
+ */
+
+@RequiredArgsConstructor
+@Service
+public class PartnerFindService {
+
+    private final PartnerFindRepository partnerFindRepository;
+
+    public List<PartnerFindDTO.Summary> searchPartnerCandidateList(PartnerFindDTO.Search request){
+        return partnerFindRepository.searchPartnerCandidateList(request);
+    }
+
+}
