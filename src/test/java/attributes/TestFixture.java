0b23de7703b9a342a87147f32f8a3921a1692fcb
tlatmsrud <tlatmsrud@naver.com>
2023-07-12 01:47:47 +0900

    :art: Add-Test-Fixture

--- src/test/java/attributes/TestFixture.java
+++ src/test/java/attributes/TestFixture.java
@@ -2,13 +2,12 @@ package attributes;
 
 import com.hellparty.domain.MemberEntity;
 import com.hellparty.domain.PartnerRequestEntity;
-import com.hellparty.dto.MemberDTO;
-import com.hellparty.dto.PartnerDTO;
-import com.hellparty.dto.PartnerRequestDTO;
+import com.hellparty.dto.*;
 import com.hellparty.enums.*;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.sql.Time;
 import java.util.Arrays;
 import java.util.List;
 
@@ -93,5 +92,30 @@ public interface TestFixture{
             new PartnerDTO(1L, "파트너1", ExecStatus.W)
             ,new PartnerDTO(2L, "파트너2", ExecStatus.I)
             ,new PartnerDTO(3L, "파트너3", ExecStatus.H)
+    );
+    ExecDayDTO EXEC_DAY_DTO = new ExecDayDTO(false, true, true, true, true, true, false);
+    PartnerFindDTO.Search PARTNER_SEARCH_DTO_ONLY_AGE_CONDITION = PartnerFindDTO.Search.builder()
+            .fromAge(20)
+            .toAge(30)
+            .sex(null)
+            .mbti(null)
+            .execArea(null)
+            .execDay(EXEC_DAY_DTO)
+            .execStartTime(null)
+            .execEndTime(null)
+            .build();
+
+
+
+    List<PartnerFindDTO.Summary> PARTNER_FIND_DTO_SUMMARY_LIST = Arrays.asList(
+            new PartnerFindDTO.Summary(11L, "닉네임1", 20, Sex.M, "profileUrl1","bodyProfileUrl1",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("21:00:00"))
+            ,new PartnerFindDTO.Summary(12L, "닉네임2", 21, Sex.M, "profileUrl2","bodyProfileUrl2",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("20:00:00"))
+            ,new PartnerFindDTO.Summary(13L, "닉네임3", 22, Sex.W, "defaultProfileUrl","defaultBodyProfileUrl",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("20:00:00"))
+            ,new PartnerFindDTO.Summary(14L, "닉네임4", 23, Sex.W, "profileUrl3","bodyProfileUrl3",EXEC_DAY_DTO, Time.valueOf("13:00:00"), Time.valueOf("15:00:00"))
+            ,new PartnerFindDTO.Summary(15L, "닉네임5", 24, Sex.W, "profileUrl4","bodyProfileUrl4",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("21:00:00"))
+            ,new PartnerFindDTO.Summary(16L, "닉네임6", 25, Sex.W, "profileUrl5","bodyProfileUrl5",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("21:00:00"))
+            ,new PartnerFindDTO.Summary(17L, "닉네임7", 26, Sex.M, "profileUrl6","bodyProfileUrl6",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("21:00:00"))
+            ,new PartnerFindDTO.Summary(18L, "닉네임8", 27, Sex.M, "profileUrl7","bodyProfileUrl7",EXEC_DAY_DTO, Time.valueOf("19:00:00"), Time.valueOf("21:00:00"))
+            ,new PartnerFindDTO.Summary(19L, "닉네임9", 28, Sex.M, "profileUrl8","bodyProfileUrl8",EXEC_DAY_DTO, Time.valueOf("10:00:00"), Time.valueOf("13:00:00"))
     );
 }
