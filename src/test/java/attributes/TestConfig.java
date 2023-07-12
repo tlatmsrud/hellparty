e602d570d0ee6c1a3f51b17ae49f5dfaf9439efb
tlatmsrud <tlatmsrud@naver.com>
2023-07-13 03:05:48 +0900

    :sparkles: Create-data-sql-for-test
    Repository에 대한 Junit 테스트 시 JpaQueryFactory Bean을 찾지 못하여 따로 생성한 설정 클래스.

---
+++ src/test/java/attributes/TestConfig.java
@@ -0,0 +1,27 @@
+package attributes;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * title        : 테스트 설정
+ * author       : sim
+ * date         : 2023-07-12
+ * description  : 테스트 설정 클래스
+ */
+
+
+@TestConfiguration
+public class TestConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
