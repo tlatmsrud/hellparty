7b09a0f87353b9ab10990d64a7f471f332356804
tlatmsrud <tlatmsrud@naver.com>
2023-07-14 17:17:20 +0900

    :sparkles: Test-PartnerRequestRepository

---
+++ src/test/java/com/hellparty/repository/PartnerRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.hellparty.repository;
+
+import attributes.TestConfig;
+import attributes.TestFixture;
+import com.hellparty.dto.PartnerDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-14
+ * description  :
+ */
+
+
+@Import(TestConfig.class)
+@DataJpaTest
+@TestPropertySource(properties = { "spring.config.location=classpath:application-db.yml" })
+public class PartnerRepositoryTest implements TestFixture {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Test
+    @DisplayName("파트너 리스트 조회")
+    void getPartnerList(){
+        List<PartnerDTO> list = partnerRepository.getPartnerList(LOGIN_MEMBER_ID);
+        assertThat(list).hasSize(2);
+        assertThat(list.get(0).getNickname()).isEqualTo("테스터1");
+        assertThat(list.get(1).getNickname()).isEqualTo("테스터2");
+    }
+}
