5eaf2143aca0c9f4546557488f634d8f2498dc3a
tlatmsrud <tlatmsrud@naver.com>
2023-07-14 18:05:47 +0900

    :test_tube: deletePartner

--- src/test/java/com/hellparty/service/PartnerServiceTest.java
+++ src/test/java/com/hellparty/service/PartnerServiceTest.java
@@ -1,16 +1,22 @@
 package com.hellparty.service;
 
 import attributes.TestFixture;
+import com.hellparty.domain.PartnerEntity;
 import com.hellparty.dto.PartnerDTO;
+import com.hellparty.exception.NotFoundException;
 import com.hellparty.repository.PartnerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * title        :
@@ -27,13 +33,32 @@ class PartnerServiceTest implements TestFixture {
     void setUp(){
         given(partnerRepository.getPartnerList(LOGIN_MEMBER_ID))
                 .willReturn(PARTNER_DTO_LIST_FOR_LOGIN_MEMBER);
+
+        given(partnerRepository.findByMemberIdAndPartnerId(LOGIN_MEMBER_ID, VALID_MEMBER_ID))
+                .willReturn(VALID_PARTNER_ENTITY);
     }
     @Test
-    void getPartnerListWithValidMemberId() {
+    @DisplayName("로그인 사용자에 대한 파트너 리스트 조회")
+    void getPartnerListWithValidLoginMember() {
         List<PartnerDTO> partnerList = partnerService.getPartnerList(LOGIN_MEMBER_ID);
 
         assertThat(partnerList.get(0).getNickname()).isEqualTo("파트너1");
         assertThat(partnerList.get(1).getNickname()).isEqualTo("파트너2");
         assertThat(partnerList.get(2).getNickname()).isEqualTo("파트너3");
+    }
+
+    @Test
+    @DisplayName("로그인 사용자의 파트너 삭제")
+    void deletePartnerWithLoginMember(){
+        partnerService.deletePartner(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
+        verify(partnerRepository).findByMemberIdAndPartnerId(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
+        verify(partnerRepository).delete(any(PartnerEntity.class));
+    }
+
+    @Test
+    @DisplayName("로그인 사용자의 유효하지 않은 파트너 삭제")
+    void deletePartnerToOtherMember(){
+        assertThatThrownBy(() -> partnerService.deletePartner(LOGIN_MEMBER_ID, INVALID_MEMBER_ID))
+                .isInstanceOf(NotFoundException.class);
     }
 }
