6522c697329cdacaf2c46919ec851147fc251840
tlatmsrud <tlatmsrud@naver.com>
2023-07-14 20:49:42 +0900

    :art: Add-saveRefreshToken

--- src/main/java/com/hellparty/service/TokenService.java
+++ src/main/java/com/hellparty/service/TokenService.java
@@ -3,9 +3,11 @@ package com.hellparty.service;
 import com.hellparty.dto.TokenDTO;
 import com.hellparty.jwt.JwtProvider;
 import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * title        : 토큰 서비스
@@ -20,6 +22,13 @@ public class TokenService {
 
     private final JwtProvider jwtProvider;
 
+    private final RedisTemplate<String,String> redisTemplate;
+
+    /**
+     * 리프레시 토큰 갱신
+     * @param refreshToken - 리프레시 토큰
+     * @return 갱신된 리프레시 토큰
+     */
     public TokenDTO renewToken(String refreshToken){
 
         Map<String, Object> claims = jwtProvider.parseJwtToken(refreshToken);
@@ -31,5 +40,14 @@ public class TokenService {
                 .accessToken(renewAccessToken)
                 .refreshToken(renewRefreshToken)
                 .build();
+    }
+
+    /**
+     * 리프레시 토큰 레디스 저장
+     * @param memberId - 사용자 ID (key)
+     * @param refreshToken - 리프레시 토큰 (value)
+     */
+    public void saveRefreshToken(Long memberId, String refreshToken){
+        redisTemplate.opsForValue().set(memberId.toString(), refreshToken,7, TimeUnit.DAYS);
     }
 }
