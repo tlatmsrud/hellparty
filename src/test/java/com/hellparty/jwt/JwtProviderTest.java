7b0f38fba9bc27cf1745cb97b1ba14b9d589f45b
tlatmsrud <tlatmsrud@naver.com>
2023-07-17 22:51:44 +0900

    :white_check_mark: Test

--- src/test/java/com/hellparty/jwt/JwtProviderTest.java
+++ src/test/java/com/hellparty/jwt/JwtProviderTest.java
@@ -1,7 +1,11 @@
 package com.hellparty.jwt;
 
+import attributes.TestFixture;
 import io.jsonwebtoken.JwtException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
@@ -9,40 +13,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
-class JwtProviderTest {
+class JwtProviderTest implements TestFixture {
 
-    private final String secretKey = "123456789012345678901234567890";
-    JwtProvider jwtProvider = new JwtProvider(secretKey);
-
-
-    private final static Map<String, Object> CLAIMS = Map.of(
-            "id",1L,"email","test@naver.com","nickname","테스터");
-
-    private final static Long CLAIM_MEMBER_ID = 1L;
-    private final static String VALID_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9" +
-            ".eyJzdWIiOiJyZWZyZXNoVG9rZW4iLCJpZCI6MSwiaWF0IjoxNjg4MjI4NzgwLCJleHAiOjIyOTMwMjg3ODB9" +
-            ".I9GzqNott_LJE7xIhhJ4ZuoARNHwoDyAgiUZ8tgVGc4";
-
-    private final static String INVALID_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9" +
-            ".eyJzdWIiOiJyZWZyZXNoVG9rZW4iLCJpZCI6MSwiaWF0IjoxNjg4MjI4NzgwLCJleHAiOjIyOTMwMjg3ODB9" +
-            ".I9GzqNott_LJE7xIhhJ4ZuoARNHwoDyAgiUZ8tgVGc4_INVALID";
+    JwtProvider jwtProvider = new JwtProvider(JWT_SECRET_KEY);
 
     @Test
+    @DisplayName("엑세스 토큰 생성")
     void generateAccessToken() {
 
-        String token = jwtProvider.generateAccessToken(CLAIM_MEMBER_ID);
+        String token = jwtProvider.generateAccessToken(CLAIMS);
         assertThat(token).contains("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9");
 
     }
 
     @Test
+    @DisplayName("리프레시 토큰 생성")
     void generateRefreshToken() {
 
-        String token = jwtProvider.generateRefreshToken(CLAIM_MEMBER_ID);
+        String token = jwtProvider.generateRefreshToken(CLAIMS);
         assertThat(token).contains("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9");
 
     }
     @Test
+    @DisplayName("유효한 엑세스 토큰 파싱")
     void parseJwtTokenWithValidToken() {
 
         assertThat(jwtProvider.parseJwtToken(VALID_ACCESS_TOKEN))
@@ -50,10 +43,36 @@ class JwtProviderTest {
     }
 
     @Test
+    @DisplayName("유효하지 않은 엑세스 토큰 파싱")
     void parseJwtTokenWithInvalidToken() {
 
         assertThatThrownBy(()-> jwtProvider.parseJwtToken(INVALID_ACCESS_TOKEN))
                 .isInstanceOf(JwtException.class);
 
+    }
+
+    @Test
+    @DisplayName("유효한 Authorization 헤더의 엑세스 토큰 추출")
+    void extractAccessTokenWithValidAuthorization(){
+        assertThat(jwtProvider.extractAccessToken(VALID_AUTHORIZATION))
+                .isEqualTo(VALID_ACCESS_TOKEN);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {INVALID_AUTHORIZATION, "", VALID_ACCESS_TOKEN})
+    @DisplayName("유효하지 않은 Authorization 헤더의 엑세스 토큰 추출")
+    void extractAccessTokenWithInvalidAuthorization(String authorization){
+        assertThatThrownBy(() -> jwtProvider.extractAccessToken(authorization))
+                .isInstanceOf(JwtException.class);
+
+    }
+
+    @Test
+    @DisplayName("클레임 생성")
+    void generateClaims(){
+        Map<String, Object> claims = jwtProvider.generateClaims(VALID_MEMBER_ENTITY);
+        assertThat(claims.get("id")).isEqualTo(VALID_MEMBER_ENTITY.getId());
+        assertThat(claims.get("email")).isEqualTo(VALID_MEMBER_ENTITY.getEmail());
+        assertThat(claims.get("nickname")).isEqualTo(VALID_MEMBER_ENTITY.getNickname());
     }
 }
