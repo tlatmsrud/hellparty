4029e38069bd7e438ab42c8d47355ff7a9dcf082
tlatmsrud <tlatmsrud@naver.com>
2023-07-27 18:00:55 +0900

    :memo: Add-comment

--- src/main/java/com/hellparty/controller/TokenController.java
+++ src/main/java/com/hellparty/controller/TokenController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * title        :
+ * title        : 토큰 컨트롤러
  * author       : sim
  * date         : 2023-07-03
- * description  :
+ * description  : 토큰 관리 컨트롤러 클래스
  */
 
 @RestController
@@ -23,6 +23,11 @@ public class TokenController {
 
     private final TokenService tokenService;
 
+    /**
+     * 리프레시 토큰을 통한 액세스 토큰 및 리프레시 토큰 갱신
+     * @param refreshToken - 리프레시 토큰
+     * @return 토큰 객체 (AT + RT)
+     */
     @GetMapping("/refresh-token")
     public TokenDTO getRefreshToken(@RequestParam("refreshToken") String refreshToken){
         return tokenService.renewToken(refreshToken);
