e46afe3aa8b3d7a52f08c4c7cc96cc8c0d2691d1
tlatmsrud <tlatmsrud@naver.com>
2023-07-29 07:50:15 +0900

    :fire: Remove-test-method-because-refactoring

--- src/test/java/com/hellparty/service/PartnerServiceTest.java
+++ src/test/java/com/hellparty/service/PartnerServiceTest.java
@@ -1,28 +1,21 @@
 package com.hellparty.service;
 
 import attributes.TestFixture;
-import com.hellparty.domain.PartnerEntity;
-import com.hellparty.dto.PartnerDTO;
-import com.hellparty.exception.NotFoundException;
 import com.hellparty.repository.PartnerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
- * title        :
+ * title        : PartnerService 테스트
  * author       : sim
  * date         : 2023-07-09
- * description  : 파트너 서비스 테스트
+ * description  : PartnerService 테스트
  */
 class PartnerServiceTest implements TestFixture {
 
@@ -34,34 +27,20 @@ class PartnerServiceTest implements TestFixture {
         given(partnerRepository.getPartnerList(LOGIN_MEMBER_ID))
                 .willReturn(PARTNER_DTO_LIST_FOR_LOGIN_MEMBER);
 
-        given(partnerRepository.findByMemberIdAndPartnerId(LOGIN_MEMBER_ID, VALID_MEMBER_ID))
-                .willReturn(VALID_PARTNER_ENTITY);
-
-        given(partnerRepository.findByMemberIdAndPartnerId(LOGIN_MEMBER_ID, INVALID_MEMBER_ID))
-                .willThrow(new NotFoundException("요청 정보를 찾을 수 없습니다. 관리자에게 문의해주세요."));
     }
     @Test
     @DisplayName("로그인 사용자에 대한 파트너 리스트 조회")
     void getPartnerListWithValidLoginMember() {
-        List<PartnerDTO> partnerList = partnerService.getPartnerList(LOGIN_MEMBER_ID);
+        assertThat(partnerService.getPartnerList(LOGIN_MEMBER_ID))
+                .isEqualTo(PARTNER_DTO_LIST_FOR_LOGIN_MEMBER);
 
-        assertThat(partnerList.get(0).getNickname()).isEqualTo("파트너1");
-        assertThat(partnerList.get(1).getNickname()).isEqualTo("파트너2");
-        assertThat(partnerList.get(2).getNickname()).isEqualTo("파트너3");
     }
 
     @Test
     @DisplayName("로그인 사용자의 파트너 삭제")
     void deletePartnerWithLoginMember(){
         partnerService.deletePartner(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
-        verify(partnerRepository).findByMemberIdAndPartnerId(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
-        verify(partnerRepository).delete(any(PartnerEntity.class));
-    }
 
-    @Test
-    @DisplayName("로그인 사용자의 유효하지 않은 파트너 삭제")
-    void deletePartnerToOtherMember(){
-        assertThatThrownBy(() -> partnerService.deletePartner(LOGIN_MEMBER_ID, INVALID_MEMBER_ID))
-                .isInstanceOf(NotFoundException.class);
+        verify(partnerRepository).deleteByMemberIdAndPartnerId(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
     }
 }
