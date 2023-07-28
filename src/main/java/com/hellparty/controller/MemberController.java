d91fefe10ea6def44fdf042f040325fd07a1071a
tlatmsrud <tlatmsrud@naver.com>
2023-07-29 07:23:46 +0900

    :bulb: Add-comment

--- src/main/java/com/hellparty/controller/MemberController.java
+++ src/main/java/com/hellparty/controller/MemberController.java
@@ -27,65 +27,75 @@ public class MemberController {
 
     /**
      * 사용자 기본정보 조회
-     * @param id - 사용자 ID
+     * @param loginId - 로그인 ID
      * @return 사용자 기본정보
      */
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public MemberDTO getDetail(@LoginMemberId Long id){
+    public MemberDTO getDetail(@LoginMemberId Long loginId){
 
-        return memberService.getDetail(id);
+        return memberService.getDetail(loginId);
 
     }
 
     /**
      * 사용자 헬스정보 조회
-     * @param id - 사용자 ID
+     * @param loginId - 로그인 ID
      * @return 헬스정보
      */
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
-    public MemberHealthDTO getHealthDetail(@LoginMemberId Long id){
+    public MemberHealthDTO getHealthDetail(@LoginMemberId Long loginId){
 
-        return memberService.getHealthDetail(id);
+        return memberService.getHealthDetail(loginId);
 
     }
 
     /**
      * 사용자 기본정보 업데이트
-     * @param id - 사용자 ID
+     * @param loginId - 로그인 ID
      * @param request - 업데이트 데이터
      */
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateDetail(@LoginMemberId Long id, @Valid @RequestBody MemberDTO.Update request){
+    public void updateDetail(@LoginMemberId Long loginId, @Valid @RequestBody MemberDTO.Update request){
 
-        memberService.updateDetail(id, request);
+        memberService.updateDetail(loginId, request);
     }
 
     /**
      * 사용자 헬스정보 업데이트
-     * @param id - 사용자 ID
+     * @param loginId - 로그인 ID
      * @param request - 업데이트 데이터
      */
     @PutMapping("/health")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateHealthDetail(@LoginMemberId Long id, @RequestBody MemberHealthDTO.Update request){
+    public void updateHealthDetail(@LoginMemberId Long loginId, @RequestBody MemberHealthDTO.Update request){
 
-        memberService.updateHealthDetail(id, request);
+        memberService.updateHealthDetail(loginId, request);
     }
 
+    /**
+     * 운동 상태 업데이트
+     * @param loginId - 로그인 ID
+     * @param status - 운동상태
+     */
     @PatchMapping("/exec-status")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateExecStatus(@LoginMemberId Long id, @RequestParam ExecStatus status){
+    public void updateExecStatus(@LoginMemberId Long loginId, @RequestParam ExecStatus status){
 
-        memberService.updateExecStatus(id,status);
+        memberService.updateExecStatus(loginId,status);
     }
 
+    /**
+     * 파트너 구함 상태 업데이트
+     * @param loginId - 로그인 ID
+     * @param status - 파트너 구함 상태
+     */
     @PatchMapping("/partner-find-status")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updatePartnerFindStatus(@LoginMemberId Long id, @RequestParam PartnerFindStatus status){
+    public void updatePartnerFindStatus(@LoginMemberId Long loginId, @RequestParam PartnerFindStatus status){
 
-        memberService.updatePartnerFindStatus(id,status);
+        memberService.updatePartnerFindStatus(loginId,status);
     }
 }
