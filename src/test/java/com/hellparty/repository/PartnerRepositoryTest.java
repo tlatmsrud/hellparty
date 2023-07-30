b2fdcae4bc21afd7a63d163723369bd9e6885ea0
tlatmsrud <tlatmsrud@naver.com>
2023-07-30 16:59:25 +0900

    :white_check_mark: Test-deletePartnership

--- src/test/java/com/hellparty/repository/PartnerRepositoryTest.java
+++ src/test/java/com/hellparty/repository/PartnerRepositoryTest.java
@@ -3,6 +3,7 @@ package com.hellparty.repository;
 import attributes.TestConfig;
 import attributes.TestFixture;
 import com.hellparty.dto.PartnerDTO;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PartnerRepositoryTest implements TestFixture {
 
     @Autowired
+    private EntityManager entityManager;
+    @Autowired
     private PartnerRepository partnerRepository;
 
     @Test
@@ -37,5 +40,20 @@ public class PartnerRepositoryTest implements TestFixture {
         assertThat(list).hasSize(2);
         assertThat(list.get(0).getNickname()).isEqualTo("테스터1");
         assertThat(list.get(1).getNickname()).isEqualTo("테스터2");
+    }
+
+    @Test
+    @DisplayName("파트너 관계 삭제")
+    void deletePartnership(){
+
+        // partner1과 partner2는 파트너쉽 관계로, 서로에 대한 파트너 엔티티를 갖고있다.
+        assertThat(partnerRepository.findById(3L).isPresent()).isTrue();// LOGIN_MEMBER -> PARTNER_ID_OF_LOGIN_MEMBER
+        assertThat(partnerRepository.findById(4L).isPresent()).isTrue(); // PARTNER_ID_OF_LOGIN_MEMBER -> LOGIN_MEMBER
+
+        partnerRepository.deletePartnership(LOGIN_MEMBER_ID, PARTNER_ID_OF_LOGIN_MEMBER);
+
+        entityManager.clear();
+        assertThat(partnerRepository.findById(3L).isPresent()).isFalse();// LOGIN_MEMBER -> PARTNER_ID_OF_LOGIN_MEMBER
+        assertThat(partnerRepository.findById(4L).isPresent()).isFalse(); // PARTNER_ID_OF_LOGIN_MEMBER -> LOGIN_MEMBER
     }
 }
