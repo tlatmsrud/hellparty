f8f522f4c4d8c7fe621cdf25714759d1eac81e0e
tlatmsrud <tlatmsrud@naver.com>
2023-08-01 07:44:52 +0900

    :sparkles: Create-Image-Exception

---
+++ src/main/java/com/hellparty/exception/ImageSaveException.java
@@ -0,0 +1,15 @@
+package com.hellparty.exception;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-08-01
+ * description  :
+ */
+public class ImageSaveException extends RuntimeException{
+
+    public ImageSaveException(String message){
+        super(message);
+    }
+
+}
