55a543f3a05c68cfc3e23ee265ea212c62f71fa5
tlatmsrud <tlatmsrud@naver.com>
2023-08-03 23:03:08 +0900

    :wrench: Add-Jasypt-setting
    Jasypt 관련 설정

--- src/main/java/com/hellparty/HellpartyApplication.java
+++ src/main/java/com/hellparty/HellpartyApplication.java
@@ -1,11 +1,13 @@
 package com.hellparty;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEncryptableProperties
 public class HellpartyApplication {
 
 	public static void main(String[] args) {
