a6d9c42f771eee9d4536c114b8a54814604eceeb
tlatmsrud <tlatmsrud@naver.com>
2023-08-04 07:52:53 +0900

    :wrench: Add-Redis-Passowrd-set

--- src/main/java/com/hellparty/config/RedisConfig.java
+++ src/main/java/com/hellparty/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -32,9 +33,15 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("{spring.data.redis.password}")
+    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setPassword(password);
+        config.setHostName(host);
+        config.setPort(port);
         return new LettuceConnectionFactory(host, port);
     }
 
