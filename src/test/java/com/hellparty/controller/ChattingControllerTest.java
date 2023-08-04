fc467a2d69ab3e5d3993effe6a6d37c7a3416cad
tlatmsrud <tlatmsrud@naver.com>
2023-08-04 19:32:12 +0900

    :white_check_mark: Test-ChattingController
    1. 상대 유저에 대한 채팅방 ID 조회
     있을 경우 기존 채팅방 조회, 없을 경우 새로생성
    
    2. 채팅방 채팅내역 조회

--- src/test/java/com/hellparty/controller/ChattingControllerTest.java
+++ src/test/java/com/hellparty/controller/ChattingControllerTest.java
@@ -9,33 +9,46 @@ import com.hellparty.service.ChattingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * title        :
+ * title        : ChattingControllerTest
  * author       : sim
  * date         : 2023-07-26
- * description  :
+ * description  : ChattingControllerTest
  */
 
 @WebMvcTest(ChattingController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 @Import(HttpEncodingAutoConfiguration.class)
+@ExtendWith(RestDocumentationExtension.class)
 
 class ChattingControllerTest implements TestFixture {
 
-    @Autowired
     private MockMvc mockMvc;
 
     @MockBean
@@ -43,7 +56,13 @@ class ChattingControllerTest implements TestFixture {
 
     private ObjectMapper objectMapper;
     @BeforeEach
-    void setUp(){
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation){
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter("UTF-8", true);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .addFilter(encodingFilter)
+                .build();
+
         setObjectMapper();
         given(chattingService.getChattingRoom(LOGIN_MEMBER_ID, VALID_MEMBER_ID))
                 .willReturn(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER);
@@ -62,9 +81,19 @@ class ChattingControllerTest implements TestFixture {
     @DisplayName("상대 유저에 대한 채팅방 ID 조회")
     void getChattingRoom() throws Exception {
         mockMvc.perform(
-                get("/api/chatting/room/{memberId}",VALID_MEMBER_ID))
+                RestDocumentationRequestBuilders.get("/api/chatting/room/{memberId}",VALID_MEMBER_ID)
+                        .header("Authorization", "Bearer JWT_ACCESS_TOKEN"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER.toString()));
+                .andExpect(content().string(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER.toString()))
+                .andDo(print())
+                .andDo(document(
+                        "getChattingRoom"
+                        , requestHeaders(
+                                headerWithName("Authorization").description("JWT_ACCESS_TOKEN"))
+                        , pathParameters(
+                                parameterWithName("memberId").description("상대방 사용자 ID"))
+                        , responseBody())
+                );
     }
 
     @Test
@@ -72,8 +101,23 @@ class ChattingControllerTest implements TestFixture {
     @DisplayName("채팅방 ID에 대한 채팅내역 조회")
     void getChatHistory() throws Exception {
         mockMvc.perform(
-                        get("/api/chatting/history/{roomId}",CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER))
+                RestDocumentationRequestBuilders.get("/api/chatting/history/{roomId}",CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER)
+                        .header("Authorization", "Bearer JWT_ACCESS_TOKEN"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(CHATTING_HISTORY_DTO_LIST)));
+                .andExpect(content().string(objectMapper.writeValueAsString(CHATTING_HISTORY_DTO_LIST)))
+                .andDo(print())
+                .andDo(document(
+                        "getChatHistory"
+                        , requestHeaders(
+                                headerWithName("Authorization").description("JWT_ACCESS_TOKEN"))
+                        , pathParameters(
+                                parameterWithName("roomId").description("채팅방 ID"))
+                        , responseFields(
+                                fieldWithPath("[]").description("채팅내역 리스트")
+                                ,fieldWithPath("[].memberId").description("사용자 ID")
+                                ,fieldWithPath("[].message").description("메시지")
+                                ,fieldWithPath("[].time").description("시간"))
+                        )
+                );
     }
 }
