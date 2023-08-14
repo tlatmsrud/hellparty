37446bd9e09359cb814543e14847465e725c5c00
tlatmsrud <tlatmsrud@naver.com>
2023-08-15 03:27:52 +0900

    :necktie: Add-registrationPartner
    파트너 등록하는 비지니스 로직 생성

--- src/main/java/com/hellparty/service/PartnerService.java
+++ src/main/java/com/hellparty/service/PartnerService.java
@@ -1,6 +1,10 @@
 package com.hellparty.service;
 
+import com.hellparty.domain.MemberEntity;
+import com.hellparty.domain.PartnerEntity;
 import com.hellparty.dto.PartnerDTO;
+import com.hellparty.exception.NotFoundException;
+import com.hellparty.repository.MemberRepository;
 import com.hellparty.repository.PartnerRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +28,7 @@ public class PartnerService {
 
     private final PartnerRepository partnerRepository;
 
+    private final MemberRepository memberRepository;
     /**
      * 파트너 리스트 조회
      * @param loginId - 로그인 ID
@@ -40,5 +45,25 @@ public class PartnerService {
      */
     public void deletePartnership(Long loginId, Long partnerId) {
         partnerRepository.deletePartnership(loginId, partnerId);
+    }
+
+    /**
+     * 파트너 등록
+     * @param member1 - 사용자 ID
+     * @param member2 - 사용자 ID
+     */
+    public void registrationPartner(Long member1, Long member2){
+
+        MemberEntity memberEntity1 = memberRepository.findById(member1)
+                .orElseThrow(() -> new NotFoundException("등록하고자 하는 파트너 정보를 찾을 수 없습니다."));
+
+        MemberEntity memberEntity2 = memberRepository.findById(member2)
+                .orElseThrow(() -> new NotFoundException("등록하고자 하는 파트너 정보를 찾을 수 없습니다."));
+
+        PartnerEntity partnerEntity1 = new PartnerEntity(memberEntity1, memberEntity2);
+        PartnerEntity partnerEntity2 = new PartnerEntity(memberEntity2, memberEntity1);
+
+        partnerRepository.save(partnerEntity1);
+        partnerRepository.save(partnerEntity2);
     }
 }
