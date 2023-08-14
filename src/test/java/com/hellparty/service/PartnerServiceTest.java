3688c7c04f149cd45e7a32c8e1de22df3c1cd6e2
tlatmsrud <tlatmsrud@naver.com>
2023-08-15 03:29:10 +0900

    :white_check_mark: Test-registrationPartner

--- src/test/java/com/hellparty/service/PartnerServiceTest.java
+++ src/test/java/com/hellparty/service/PartnerServiceTest.java
@@ -1,15 +1,21 @@
 package com.hellparty.service;
 
 import attributes.TestFixture;
+import com.hellparty.domain.PartnerEntity;
+import com.hellparty.exception.NotFoundException;
+import com.hellparty.repository.MemberRepository;
 import com.hellparty.repository.PartnerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * title        : PartnerService 테스트
@@ -20,13 +26,19 @@ import static org.mockito.Mockito.verify;
 class PartnerServiceTest implements TestFixture {
 
     private final PartnerRepository partnerRepository = mock(PartnerRepository.class);
-    private final PartnerService partnerService = new PartnerService(partnerRepository);
+    private final MemberRepository memberRepository = mock(MemberRepository.class);
+    private final PartnerService partnerService = new PartnerService(partnerRepository ,memberRepository);
 
     @BeforeEach
     void setUp(){
         given(partnerRepository.getPartnerList(LOGIN_MEMBER_ID))
                 .willReturn(PARTNER_DTO_LIST_FOR_LOGIN_MEMBER);
 
+        given(memberRepository.findById(LOGIN_MEMBER_ID))
+                .willReturn(Optional.of(LOGIN_MEMBER_ENTITY));
+
+        given(memberRepository.findById(VALID_MEMBER_ID))
+                .willReturn(Optional.of(VALID_MEMBER_ENTITY));
     }
     @Test
     @DisplayName("로그인 사용자에 대한 파트너 리스트 조회")
@@ -41,5 +53,21 @@ class PartnerServiceTest implements TestFixture {
     void deletePartnerWithLoginMember(){
         partnerService.deletePartnership(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
         verify(partnerRepository).deletePartnership(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
+    }
+
+    @Test
+    @DisplayName("파트너 등록")
+    void registrationPartner(){
+        partnerService.registrationPartner(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
+        verify(memberRepository).findById(LOGIN_MEMBER_ID);
+        verify(memberRepository).findById(VALID_MEMBER_ID);
+        verify(partnerRepository,times(2)).save(any(PartnerEntity.class));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 파트너 등록")
+    void registrationPartnerWithInvalidMemberId(){
+        assertThatThrownBy(()->partnerService.registrationPartner(LOGIN_MEMBER_ID, INVALID_MEMBER_ID))
+                .isInstanceOf(NotFoundException.class);
     }
 }
